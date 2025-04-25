2cd4d3014ef805d25e510c58dacda844edfdc795
EchoedDestiny428 <zacharyng428@gmail.com>
2025-04-25 17:38:56 +0800

    stuff

--- src/PurePursuitController.cpp
+++ src/PurePursuitController.cpp
@@ -1,5 +1,6 @@
 // move this into main when you are done testing.
-// dont screw up, Osiris.
+// dont expect this to be finished
+// Osi don't stres over this.
 
 #include <vector>
 #include <iostream>
@@ -7,6 +8,12 @@
 
 class PurePursuitController {
     public:
+        struct Pose {
+            float x;
+            float y;
+        } currentPose;
+
+
         void setPose(float x, float y, float heading) {
             currentPose.x = x;
             currentPose.y = y;
@@ -16,12 +23,57 @@ class PurePursuitController {
             path.push_back({x, y});
         }
 
-    private:
-        struct Pose {
-            float x;
-            float y;
-        } currentPose;
+        void setLookAheadDistance(float distance) {
+            lookAheadDistance = distance;
+        }
 
+        int sign(float number) {
+            if (number >= 0) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+
+
+        std::vector<Pose> line_circle_intersection(std::vector<Pose> currentPos, std::vector<Pose> pt1, std::vector<Pose> pt2, float lookAheadDistance) {
+            bool intersectFound = false;
+
+            float currentX = currentPos[0].x;
+            float currentY = currentPos[0].y;
+            float x1 = pt1[0].x;
+            float y1 = pt1[0].y;
+            float x2 = pt2[0].x;
+            float y2 = pt2[0].y; 
+
+            float x1_offset = x1 - currentX;
+            float y1_offset = y1 - currentY;
+            float x2_offset = x2 - currentX;
+            float y2_offset = y2 - currentY;
+
+            float dx = x2_offset - x1_offset;
+            float dy = y2_offset - y1_offset;
+            float dr = std::sqrt(pow(dx, 2) + pow(dy, 2));
+            float D = x1_offset*y2_offset - x2_offset*y1_offset;
+            float discriminant = (pow(lookAheadDistance, 2)) * (pow(dr, 2)) - pow(D, 2);
+
+            // Check if the line intersects with the circle
+            float discriminant = b * b - 4 * a * c;
+            
+            if (line_circle_intersection(currentPose.x, currentPose.y, path[currentPathIndex].x, path[currentPathIndex].y, currentPose.x, currentPose.y) >= 0) {
+                sol_x1 = (D * dy + sign(dy) * dx * np.sqrt(discriminant)) / dr**2;
+                sol_x2 = (D * dy - sign(dy) * dx * np.sqrt(discriminant)) / dr**2;
+                sol_y1 = (- D * dx + abs(dy) * np.sqrt(discriminant)) / dr**2;
+                sol_y2 = (- D * dx - abs(dy) * np.sqrt(discriminant)) / dr**2 ; 
+            } else {
+                return;
+            }
+
+        
+            return;
+        }
+
+    private:
         int currentPathIndex = 0;
 
         std::vector<Pose> path;
